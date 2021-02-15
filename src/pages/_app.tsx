d7b998d9f451865a46328baa689e80c20a93d5cf
Clayton <corman.dev@gmail.com>
2021-02-15 14:53:03 -0500

    🎨 implement initial tailwind integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import { FC } from 'react';
 
+import '../styles/global.css';
+
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react';
 
 const HomePage: FC = () => {
-  return <div>Welcome to my blog!</div>;
+  return (
+    <div className="mx-auto max-w-sm">
+      <h1>Welcome to my blog!</h1>
+    </div>
+  );
 };
 
 export default HomePage;
