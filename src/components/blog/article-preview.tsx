62a4c1e5a267c5df5eff7e3c2874bae78c46c54e
Clayton <corman.dev@gmail.com>
2021-02-18 08:46:08 -0500

    📚 display post previews on blog page

---
+++ src/components/blog/article-preview.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React, { FC } from 'react';
+import { Post } from '../../interfaces/post';
+
+const ArticlePreview: FC<{ post: Post }> = ({ post }) => {
+  const title = post.frontmatter.title || post.slug;
+
+  return (
+    <article itemScope itemType="http://schema.org/Article">
+      <header>
+        <h2>
+          <Link href={`/${post.slug}`}>
+            <a itemProp="url">
+              <span itemProp="headline">{title}</span>
+            </a>
+          </Link>
+        </h2>
+        <small>{post.frontmatter.date}</small>
+      </header>
+      <section>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: post.frontmatter.description || post.excerpt,
+          }}
+          itemProp="description"
+        />
+      </section>
+    </article>
+  );
+};
+
+export default ArticlePreview;
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { GetStaticProps } from 'next';
 import Layout from '../../components/layout';
+import { getAllPosts } from '../../lib/blog';
+import { Post } from '../../interfaces/post';
+import ArticlePreview from '../../components/blog/article-preview';
 
-const BlogPage = () => {
+const BlogPage: FC<{ posts: Post[] }> = ({ posts }) => {
   return (
     <Layout>
       <h1>Here are a list of my blog posts</h1>
+      {posts.map((post) => {
+        return <ArticlePreview key={post.slug} post={post} />;
+      })}
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default BlogPage;
