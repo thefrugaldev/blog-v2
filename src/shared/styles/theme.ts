af106f6c23f752faa4d596cca2b7858c628f5ce0
Clayton <corman.dev@gmail.com>
2021-03-02 09:54:14 -0500

    :art: add theme and color palette to central location

--- src/shared/styles/theme.ts
+++ src/shared/styles/theme.ts
@@ -1,17 +1,23 @@
-// export const theme = {
-
-// }
+const colorPalette = {
+  primary: '#EC092F',
+  subhead: '#FFEC41',
+  info: '#7932EC',
+  success: '#00B87A',
+  warning: '#EC9D09',
+  dark: '#0E2430',
+  light: '#FEFEFE',
+};
 
 export const lightTheme = {
-  body: '#E2E2E2',
-  text: '#363537',
+  body: colorPalette.light,
+  text: colorPalette.dark,
   toggleBorder: '#FFF',
   gradient: 'linear-gradient(#39598A, #79D7ED)',
 };
 
 export const darkTheme = {
-  body: '#363537',
-  text: '#FAFAFA',
+  body: colorPalette.dark,
+  text: colorPalette.light,
   toggleBorder: '#6B8096',
   gradient: 'linear-gradient(#091236, #1E215D)',
 };
