a07603dcba3faaaf078fdb5b8d96f4c2a75b142c
Clayton <corman.dev@gmail.com>
2021-03-02 09:19:55 -0500

    🔧 add theme toggle functionality to app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import { AppProps } from 'next/app';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 
+import { GlobalStyles } from '../shared/styles/global';
+import { lightTheme, darkTheme } from '../shared/styles/theme';
 import '../shared/styles/global.css';
+import DarkModeToggle from '../shared/components/layout/components/dark-mode-toggle/dark-mode-toggle';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggle = (isDarkMode: boolean) => {
+    setIsDarkMode(isDarkMode);
+  };
+
+  return (
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <DarkModeToggle onToggle={handleToggle} />
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/shared/components/layout/layout.tsx
+++ src/shared/components/layout/layout.tsx
@@ -1,13 +1,9 @@
 import React, { FC } from 'react';
 import Header from './components/header/header';
 
-interface LayoutProps {
-  isDarkMode?: boolean;
-}
-
-const Layout: FC<LayoutProps> = ({ isDarkMode, children }) => {
+const Layout: FC = ({ children }) => {
   return (
-    <div className={`${isDarkMode ? 'bg-dark' : 'bg-light'}`}>
+    <div>
       <div className="max-w-screen-lg m-auto h-screen">
         <Header />
         <main>{children}</main>
