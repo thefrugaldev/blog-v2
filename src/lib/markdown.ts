f440b1ac1133a63375fb78c6072b631470f013f3
Clayton <corman.dev@gmail.com>
2021-03-15 13:44:21 -0400

    :shower: clean up unused dependencies

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,13 +1,5 @@
 import rehypePrism from '@mapbox/rehype-prism';
 import renderToString from 'next-mdx-remote/render-to-string';
-import remark from 'remark';
-import html from 'remark-html';
-
-// const markdownToHtml = async (markdown: any) => {
-//   const result = await remark().use(html).process(markdown);
-
-//   return result.toString();
-// };
 
 const markdownToHtml = async (markdown: any) => {
   const mdxSource = await renderToString(markdown, {
