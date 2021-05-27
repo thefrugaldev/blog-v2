6a2f07c7545b4e5d9fd009c01b6fc3a265303bb8
Clayton <corman.dev@gmail.com>
2021-05-27 16:39:39 -0400

    :wrench: remove tailwind and add chakra packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import { AppProps } from 'next/app';
-import { FC } from 'react';
-import '../shared/styles/global.css';
-import DarkModeToggle from '../shared/components/layout/components/dark-mode-toggle/dark-mode-toggle';
-import DarkModeProvider from '../lib/state/dark-mode';
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import { FC } from "react";
+import "../shared/styles/global.css";
+import DarkModeToggle from "../shared/components/layout/components/dark-mode-toggle/dark-mode-toggle";
+// import DarkModeProvider from "../lib/state/dark-mode";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <DarkModeProvider>
-      <DarkModeToggle />
+    // <DarkModeProvider>
+    <ChakraProvider>
+      {/* <DarkModeToggle /> */}
       <Component {...pageProps} />
-    </DarkModeProvider>
+    </ChakraProvider>
+    // </DarkModeProvider>
   );
 };
 
--- src/shared/components/layout/components/header/header.tsx
+++ src/shared/components/layout/components/header/header.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import HeaderItem from './header-item';
+import { Button, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import HeaderItem from "./header-item";
 
 const Header = () => {
+  const { toggleColorMode } = useColorMode();
+
   return (
     <header>
+      <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
       <ul className="flex">
-        <HeaderItem name="Home Page" route={'/'} />
-        <HeaderItem name="Blog" route={'/blog'} />
-        <HeaderItem name="Uses" route={'/uses'} />
-        <HeaderItem name="Projects" route={'/projects'} />
-        <HeaderItem name="About" route={'/about'} />
+        <HeaderItem name="Home Page" route={"/"} />
+        <HeaderItem name="Blog" route={"/blog"} />
+        <HeaderItem name="Uses" route={"/uses"} />
+        <HeaderItem name="Projects" route={"/projects"} />
+        <HeaderItem name="About" route={"/about"} />
       </ul>
     </header>
   );
