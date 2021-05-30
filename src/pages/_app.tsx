feb92786928531bc51dfc217f84e4582db944ee4
Clayton <corman.dev@gmail.com>
2021-05-30 09:27:14 -0400

    :wrench: completely remove tailwind and update theme to include color palette

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import "@fontsource/martel-sans";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { FC } from "react";
 import theme from "../shared/styles/theme";
+
 // import DarkModeToggle from "../shared/components/layout/components/dark-mode-toggle/dark-mode-toggle";
 // import DarkModeProvider from "../lib/state/dark-mode";
 
--- src/shared/styles/theme.ts
+++ src/shared/styles/theme.ts
@@ -1,9 +1,48 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
   useSystemColorMode: true,
 };
 
-const theme = extendTheme({ config });
+const colorPalette = {
+  // primary: "#F8425F",
+  // subhead: "#FFEC41",
+  // info: "#7932EC",
+  // darkInfo: "#B085F4",
+  // success: "#00B87A",
+  // darkSuccess: "#00CC88",
+  // warning: "#EC9D09",
+  // darkWarning: "#F8B53A",
+  light: "#FEFEFE",
+  dark: "#0E2430",
+};
+
+const theme = extendTheme({
+  config,
+  fonts: {
+    body: "Martel Sans, sans-serif",
+  },
+  styles: {
+    "*, *::after, *::before": {
+      boxSizing: "border-box",
+    },
+    global: (props) => ({
+      body: {
+        color: mode(colorPalette.dark, colorPalette.light)(props),
+        bg: mode(colorPalette.light, colorPalette.dark)(props),
+      },
+      h1: {
+        fontSize: "4xl",
+      },
+      h2: {
+        fontSize: "2xl",
+      },
+      "h3, h4, h5, h6": {
+        fontSize: "xl",
+      },
+    }),
+  },
+});
 
 export default theme;
