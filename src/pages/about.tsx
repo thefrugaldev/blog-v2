84b940adc4b95fc1bb6522693b8a20f6efebe2cb
Clayton <corman.dev@gmail.com>
2021-06-13 09:56:09 -0400

    🚿 tidy up about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,34 @@
-import React, { FC } from 'react';
-import Layout from '../shared/components/layout';
+import React, { FC } from "react";
+import { Image } from "@chakra-ui/image";
+import Layout from "../shared/components/layout";
+import { Heading, Text, VStack } from "@chakra-ui/layout";
 
 const AboutPage: FC = () => {
-  return <Layout>About page</Layout>;
+  return (
+    <Layout>
+      <VStack>
+        <Image
+          borderRadius="full"
+          boxSize="250px"
+          objectFit="cover"
+          objectPosition="center center"
+          src="/profile.jpeg"
+          alt="Clayton Orman"
+        />
+        <Heading as="h1" size="2xl">
+          Clayton Orman
+        </Heading>
+        <Text>
+          I'm a developer with over 10 years of professional experience. I
+          primarily dabble in the .Net and JavaScript realm, but always enjoy
+          learning new things (which is why I made this blog 😄). Other hobbies
+          include personal finance, bourbon, and sports (I'm a huge Georgia
+          Bulldogs 🐶 fan). Feel free to reach out to me on Twitter, Github, or
+          LinkedIn.
+        </Text>
+      </VStack>
+    </Layout>
+  );
 };
 
 export default AboutPage;
